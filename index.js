b49ed88c610a640b7f3571a41e87826bb636447d
Lukeghenco <lukeghenco@gmail.com>
2017-09-22 11:35:14 -0400

    Add solution code to pass wip-master branch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code Solution Here
+function driversWithRevenueOver (list, revenue) {
+    return list.filter(function (driver) {
+        return driver.revenue > revenue;
+    });
+}
+
+function driverNamesWithRevenueOver (list, revenue) {
+    return driversWithRevenueOver(list, revenue)
+        .map(function (driver) {
+            return driver.name;
+        });
+}
+
+function exactMatch (list, matcher) {
+    return list.filter(function (driver) {
+        let matches = false;
+        for (const key in matcher) {
+            matches = driver[key] === matcher[key];
+        }
+        return matches;
+    });
+}
+
+function exactMatchToList (list, matcher) {
+    return exactMatch(list, matcher)
+        .map(function (driver) {
+            return driver.name;
+        });
+}
