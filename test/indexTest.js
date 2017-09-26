3f4c4c6672ce70c381ff6017085dadec72582b99
Gabe Jackson <gj@mail.co.de>
2017-09-25 21:25:38 -0400

    Standardize formatting; use anonymous functions instead of arrow functions since they won't look quite so foreign to students looking at the test suite

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,10 +1,10 @@
 const expect = chai.expect;
 
-describe('drivers', () => {
+describe('index.js', function () {
   let drivers;
   let extendedDrivers;
 
-  beforeEach(() => {
+  beforeEach(function () {
     drivers = [
       {
         name: 'Sally',
@@ -15,27 +15,29 @@ describe('drivers', () => {
         revenue: 200
       },
       {
-        name: 'Jim', 
+        name: 'Jim',
         revenue: 150
       }
     ];
+
     extendedDrivers = drivers.concat({
       name: 'Sally',
       revenue: 200
     });
   });
 
-  describe('driversWithRevenueOver', () => {
-    it('returns an array of all matching drivers', () => {
+  describe('driversWithRevenueOver()', function () {
+    it('returns an array of all matching drivers', function () {
       expect(driversWithRevenueOver(drivers, 250)).to.eql([
         {
           name: 'Sally',
           revenue: 400
         }
       ]);
+
       expect(driversWithRevenueOver(drivers, 199)).to.eql([
         {
-          name: 'Sally', 
+          name: 'Sally',
           revenue: 400
         },
         {
@@ -45,25 +47,25 @@ describe('drivers', () => {
       ]);
     });
 
-    it('returns an empty array if there is no match', () => {
+    it('returns an empty array if there is no match', function () {
       expect(driversWithRevenueOver(drivers, 500)).to.eql([]);
     });
   });
 
-  describe('driverNamesWithRevenueOver', () => {
-    it('returns an array of all matching drivers names as string', () => {
+  describe('driverNamesWithRevenueOver()', function () {
+    it('returns an array of all matching drivers names as string', function () {
       expect(driverNamesWithRevenueOver(drivers, 199)).to.eql([
         'Sally', 'Annete'
       ]);
     });
 
-    it('returns an empty array if there is no match', () => {
+    it('returns an empty array if there is no match', function () {
       expect(driverNamesWithRevenueOver(drivers, 500)).to.eql([]);
     });
   });
 
-  describe('exactMatch', () => {
-    it('returns an array of all matching drivers', () => {
+  describe('exactMatch()', function () {
+    it('returns an array of all matching drivers', function () {
       expect(exactMatch(extendedDrivers, { name: 'Sally' })).to.eql([
         {
           name: 'Sally',
@@ -74,39 +76,41 @@ describe('drivers', () => {
           revenue: 200
         }
       ]);
+
       expect(exactMatch(extendedDrivers, { revenue: 200 })).to.eql([
         {
           name: 'Annete',
           revenue: 200
         },
         {
-          name: 'Sally', 
+          name: 'Sally',
           revenue: 200
         },
       ]);
     });
 
-    it('returns an empty array if there is no match', () => {
+    it('returns an empty array if there is no match', function () {
       expect(exactMatch(extendedDrivers, { revenue: 500 })).to.eql([]);
+
       expect(exactMatch(extendedDrivers, { name: 'Alex' })).to.eql([]);
     });
   });
 
-  describe('exactMatchToList', () => {
-    it('returns an array of names for all matching drivers', () => {
+  describe('exactMatchToList()', function () {
+    it('returns an array of names for all matching drivers', function () {
       expect(exactMatchToList(extendedDrivers, { name: 'Sally' })).to.eql([
         'Sally', 'Sally'
       ]);
+
       expect(exactMatchToList(extendedDrivers, { revenue: 200 })).to.eql([
         'Annete', 'Sally'
       ]);
     });
 
-    it('returns an empty array if there is no match', () => {
+    it('returns an empty array if there is no match', function () {
       expect(exactMatchToList(extendedDrivers, { revenue: 500 })).to.eql([]);
+
       expect(exactMatchToList(extendedDrivers, { name: 'Alex' })).to.eql([]);
     });
   });
 });
-
-
