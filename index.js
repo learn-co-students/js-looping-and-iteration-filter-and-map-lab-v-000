80d775cf0c01d32987b55ac46170ebf67ef47e99
smithwebtek <brad@smithwebtek.com>
2017-10-02 22:37:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution here:
+function driversWithRevenueOver(collection, revenue){
+  let newDrivers = []
+  for (const element of collection){
+    if (element.revenue > revenue){
+      newDrivers.push(element)
+    }
+  }
+  return newDrivers
+}
+
+function driverNamesWithRevenueOver(collection, revenue){
+  let newDrivers = []
+  for (const element of collection){
+    if (element.revenue > revenue){
+      newDrivers.push(element.name)
+    }
+  }
+  return newDrivers
+}
+
+function exactMatch(collection, obj){
+  let newDrivers = []
+  for (const element of collection){
+    if (element.name === obj.name){
+      newDrivers.push(element)
+    } else 
+    if (element.revenue === obj.revenue){
+      newDrivers.push(element)
+    }
+  }
+  return newDrivers
+}
+
+function exactMatchToList(collection, obj){
+  let newDrivers = []
+  for (const element of collection){
+    if (element.name === obj.name){
+      newDrivers.push(element.name)
+    } else 
+    if (element.revenue === obj.revenue){
+      newDrivers.push(element.name)
+    }
+  }
+  return newDrivers
+}
