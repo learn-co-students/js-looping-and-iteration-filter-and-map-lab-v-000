e1af855bfa70ab6df54cbf873dadd1a840d0e06a
Jdville03 <jarrel.delottinville@gmail.com>
2017-10-10 12:04:07 -0400

    Done.

--- index.js
+++ index.js
@@ -7,23 +7,25 @@ function driversWithRevenueOver (drivers, revenue) {
 }
 
 function driverNamesWithRevenueOver (drivers, revenue) {
-  return drivers.filter(function (driver) {
-    return driver.revenue > revenue;
-  }).map(function (filteredDriver) {
-    return filteredDriver.name;
-  });
+  return driversWithRevenueOver(drivers, revenue)
+    .map(function (filteredDriver) {
+      return filteredDriver.name;
+    });
 }
 
 function exactMatch (drivers, object) {
   return drivers.filter(function (driver) {
-    return object[Object.keys(object)[0]] === driver[Object.keys(object)[0]];
+    let matches = false;
+    for (const key in object) {
+      matches = driver[key] === object[key];
+    }
+    return matches;
   });
 }
 
 function exactMatchToList (drivers, object) {
-  return drivers.filter(function (driver) {
-    return object[Object.keys(object)[0]] === driver[Object.keys(object)[0]];
-  }).map(function (filteredDriver) {
-    return filteredDriver.name;
-  });
+  return exactMatch(drivers, object)
+    .map(function (filteredDriver) {
+      return filteredDriver.name;
+    });
 }
