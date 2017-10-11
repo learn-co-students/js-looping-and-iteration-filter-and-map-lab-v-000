7435ac7ee9006a387cf36b7a6d3ded0f6b40809f
halfields <hfields697@aol.com>
2017-10-11 00:24:10 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+	const newCollection = [];
+	for (const driver of drivers) {
+		if (driver.revenue > revenue) {
+			newCollection.push(driver);
+		}
+	}
+	return newCollection;
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+	const array = driversWithRevenueOver(drivers, revenue);
+	return array.map(function(driver) { return driver.name});
+}
+
+function exactMatch(list, attribute) {
+	const key = Object.keys(attribute)[0];
+return list.filter(function(item) {		
+		return item[key] === attribute[key]; 
+	});
+}
+
+function exactMatchToList(extendedDrivers, attribute) {
+	return exactMatch(extendedDrivers, attribute).map(function(driver) {return driver.name}); 
+} 
