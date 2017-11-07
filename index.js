cfde568702d38c09f5028ff2e50d6a6449385721
laramontana <larabrightside@gmail.com>
2017-11-07 19:21:31 +0300

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function driversWithRevenueOver(drivers, revenue) {
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
-  const driversWithRevenueOver = drivers.filter(function functionName(driver) { return driver.revenue > revenue });
-  return driversWithRevenueOver.map(function functionName(driver) {return driver.name});
+  return driversWithRevenueOver(drivers, revenue)
+  .map(function functionName(driver) {return driver.name});
 }
 
 function exactMatch(drivers, attributes) {
@@ -13,6 +13,6 @@ function exactMatch(drivers, attributes) {
 }
 
 function exactMatchToList(drivers, attributes) {
-  const exactMatch = drivers.filter(function (driver) { return driver[Object.keys(attributes)[0]] === Object.values(attributes)[0]});
-  return exactMatch.map(function (driver) { return driver.name})
+  return exactMatch(drivers, attributes)
+  .map(function (driver) { return driver.name})
 }
