c3c3a040bd59be8fc960bae830a5dfd911d662bd
laramontana <larabrightside@gmail.com>
2017-11-07 19:22:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function driversWithRevenueOver(drivers, revenue) {
-  return drivers.filter(function functionName(driver) { return driver.revenue > revenue });
+function driversWithRevenueOver (drivers, revenue) {
+  return drivers.filter (function functionName (driver) { return driver.revenue > revenue });
 }
 
-function driverNamesWithRevenueOver(drivers, revenue) {
-  return driversWithRevenueOver(drivers, revenue)
-  .map(function functionName(driver) {return driver.name});
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver (drivers, revenue)
+  .map(function (driver) { return driver.name });
 }
 
-function exactMatch(drivers, attributes) {
-  return drivers.filter(function (driver) {
+function exactMatch (drivers, attributes) {
+  return drivers.filter (function (driver) {
     return driver[Object.keys(attributes)[0]] === Object.values(attributes)[0]});
 }
 
-function exactMatchToList(drivers, attributes) {
-  return exactMatch(drivers, attributes)
-  .map(function (driver) { return driver.name})
+function exactMatchToList (drivers, attributes) {
+  return exactMatch (drivers, attributes)
+  .map(function (driver) { return driver.name })
 }
