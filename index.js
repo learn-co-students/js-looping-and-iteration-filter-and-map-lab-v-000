dd5d0db66a8b195a6fe8f4c7ee85e68a4288fb5e
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-11-10 19:18:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // Code your solution here:
 
 function driversWithRevenueOver (drivers, revenue) {
-  return drivers.filter(function (driver) {
-    if (driver.revenue > revenue) {
-      return Object.assign({}, {name: driver.name, revenue: driver.revenue});
-    }
-  });
+   return drivers.filter(function (driver) { return driver.revenue > revenue});
 }
 
 function driverNamesWithRevenueOver (drivers, revenue) {
