8704f46570192ea0a265925cf0335be58bb48901
kaisa <kaisa.piipari@gmail.com>
2017-11-11 11:14:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+    let driversWithRevenue = [];
+    drivers.map(function(driver) {
+        if (driver.revenue > revenue) {
+            driversWithRevenue.push(driver);
+        }
+    });
+    return driversWithRevenue;
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+    let driversWithRevenue = [];
+    drivers.map(function(driver) {
+        if (driver.revenue > revenue) {
+            driversWithRevenue.push(driver.name);
+        }
+    });
+    return driversWithRevenue;
+}
+
+function exactMatch(drivers, obj) {
+    let matchedDrivers = [];
+    drivers.map(function(driver) {
+        if ("name" in obj || "revenue" in obj) {
+            if (driver.name === obj.name || driver.revenue === obj.revenue) {
+                matchedDrivers.push(driver);
+            }
+        } else {
+            console.log("Not a matching object");
+        }
+    });
+    return matchedDrivers;
+}
+
+function exactMatchToList(drivers, obj) {
+    let matchedDrivers = [];
+    drivers.map(function(driver) {
+        if ("name" in obj || "revenue" in obj) {
+            if (driver.name === obj.name || driver.revenue === obj.revenue) {
+                matchedDrivers.push(driver.name);
+            }
+        } else {
+            console.log("Not a matching object");
+        }
+    });
+    return matchedDrivers;
+}
