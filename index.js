de9088486ebfcfec3921c318a5991b5982a50caa
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-12 16:03:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here:
+function driversWithRevenueOver (drivers, revenue) {
+  return drivers.filter(function (wealthydrivers) {
+    return wealthydrivers.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue)
+    .map(function (whatsMyName) {
+      return whatsMyName.name;
+    });
+}
+
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+
+    return matches;
+  });
+}
+
+function exactMatchToList (drivers, matcher) {
+  return exactMatch(drivers, matcher)
+    .map(function (imTheDriverYouWant) {
+      return imTheDriverYouWant.name;
+    });
+}
