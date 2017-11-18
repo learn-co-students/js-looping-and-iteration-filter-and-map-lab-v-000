07e97b35d0d867d1476fb704f38349317f263be8
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-11-17 23:23:20 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here:
+function driversWithRevenueOver(array, revenue) {
+  return array.filter(element => { return element.revenue > revenue; });
+};
+
+function driverNamesWithRevenueOver(array, revenue) {
+  return driversWithRevenueOver(array, revenue).map(function (element) { return element.name });
+};
+
+function exactMatch(array, query) {
+  return array.filter(function(element) {
+    if (query.name !== undefined) {
+      return element.name === query.name
+    } else {
+      return element.revenue === query.revenue
+    }
+  });
+};
+
+function exactMatchToList(array, query) {
+  return exactMatch(array, query).map(function (element) { return element.name });;
+}
