f48412ff2f2339526b5eea239ef2db13965add3c
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-17 20:24:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver) {
+    return driver.revenue > revenue;
+  });
+}
+
+// function driverNamesWithRevenueOver(drivers, revenue) {
+//   const foundDrivers = driversWithRevenueOver(drivers, revenue);
+
+//   return foundDrivers.map(function(driver) {
+//     return driver.name;
+//   });
+// }
+
+//refactored version
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function(driver) {
+    return driver.name;
+  });
+}
+
+
+function exactMatch(drivers, query) {
+  return drivers.filter(function(driver){
+      for (const key in driver ) {
+        if (driver[key] === query[key]) {
+          return true;
+        }
+      }
+  });
+}
+
+// function exactMatchToList(drivers, query) {
+//   const matchedDrivers = exactMatch(drivers, query);
+  
+//   return matchedDrivers.map(function(driver){
+//     return driver.name;
+//   });
+// }
+
+
+// refactored version
+function exactMatchToList(drivers, query) {
+  return exactMatch(drivers, query).map(function(driver){
+    return driver.name;
+  });
+}
+
+
+
