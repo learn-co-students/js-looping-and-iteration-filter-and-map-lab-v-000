b0fb02341cd990212577a01639cf389c2df32c29
github username <github email address>
2017-11-20 13:28:02 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here:
+
+// function driversWithRevenueOver(collection, revenue) {
+// 	return collection.filter(function(element) {
+//     return element.revenue > revenue
+//   })
+// }
+
+function driversWithRevenueOver(collection, revenue) {
+	return collection.filter((element) => element.revenue > revenue )
+}
+
+function driverNamesWithRevenueOver(collection, revenue) {
+  return driversWithRevenueOver(collection, revenue).map(function (element) { return element.name });
+};
+
+function exactMatch(array, query) {
+  return array.filter(function(element) {
+    if (query.name !== undefined) {
+      return element.name === query.name
+    } else {
+      return element.revenue === query.revenue
+    }
+  });
+};
+
+function exactMatchToList(array, query) {
+  return exactMatch(array, query).map(function (element) { return element.name });;
+}
