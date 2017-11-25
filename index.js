fa5e17529a95a2a2383bd0f3493538713dd9fcd9
Rony Boeck <ramg.9@live.com>
2017-11-25 00:20:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here:
+// Code your solution here:
+
+function driversWithRevenueOver(arr, revenue){
+  return arr.filter(function(driver){
+    return driver.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver(arr, revenue){
+  return driversWithRevenueOver(arr, revenue).map(function(driver){
+    return driver.name;
+  });
+}
+
+function exactMatch(arr, query){
+  return arr.filter(function(driver){
+    for(let prop in query){
+      if (query[prop] !== driver[prop]){
+         return false;
+      }
+    }
+    return true;
+  })
+}
+
+function exactMatchToList(arr, query){
+  return exactMatch(arr, query).map(function(driver){
+    return driver.name;
+  })
+}
