e5a1c164fb6b6c63c318a6192880f5e2d6962b86
tracy p holmes <tracypholmes@gmail.com>
2017-11-28 12:44:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function (driver) {
+    return driver.name;
+  });
+}
+
+function exactMatch(drivers, leMatch) {
+  return drivers.filter(function (driver) {
+    let match = false;
+    
+    for (const key in leMatch) {
+      match = driver[key] === leMatch[key]
+    }
+    
+    return match;
+  });
+}
+
+function exactMatchToList(drivers, leMatch) {
+  return exactMatch(drivers, leMatch).map(function (driver){
+    return driver.name;
+  });
+  
+}
