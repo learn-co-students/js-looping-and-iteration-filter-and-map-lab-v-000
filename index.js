2c3978162477dbbc736321c9b3b00e608f38807d
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-11-28 00:22:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here:
+function driversWithRevenueOver(array, revenue){
+  return array.filter(element => {return element.revenue > revenue;});
+};
+function driverNamesWithRevenueOver(array, revenue){
+  return driversWithRevenueOver(array, revenue).map(function (element){
+    return element.name;
+  });
+};
+function exactMatch(array, driver){
+  return array.filter(function(element){
+    if (driver.name !== undefined){
+      return element.name === driver.name;
+    } else {
+      return element.revenue === driver.revenue;
+    };
+  });
+};
+function exactMatchToList(array, driver){
+  return exactMatch(array, driver).map(function(element){
+    return element.name
+  });
+};
