db60d7ec9c796d3647436cce4975bad78b80ac88
curlywallst <nnoyes@comcast.net>
2017-12-03 11:34:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver) {
+    return driver.revenue > parseInt(revenue, 10)
+  })
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function(driver) {
+    return driver.name
+  })
+}
+
+function exactMatch(drivers, toMatch) {
+  return drivers.filter(function(driver) {
+    for (const attr in driver) {
+      if (driver[attr] === toMatch[attr]) {
+        return driver
+      }
+    }
+  })
+}
+
+function exactMatchToList(drivers, toMatch) {
+  return exactMatch(drivers, toMatch).map(function(driver){
+    return driver.name
+  })
+}
