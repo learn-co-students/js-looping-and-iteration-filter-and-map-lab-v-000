d2bba7fa32d91f74e5461fd8b42c8b529890b661
hyoyou <heatherhyou@gmail.com>
2017-12-14 14:17:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here:
+function driversWithRevenueOver (drivers, revenue) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver (drivers, revenue) {
+  const driversList = driversWithRevenueOver (drivers, revenue);
+  return driversList.map(function (driver) {
+    return driver.name;
+  });
+}
+
+/*
+function driverNamesWithRevenueOver (drivers, revenue) {
+  let driversList = drivers.map(function (driver) {
+    if (driver.revenue > revenue) {
+      return driver.name;
+    }
+  });
+  return driversList;
+}
+-> using map returns undefined when condition evaluates to false
+*/
+
+function exactMatch (drivers, value) {
+  return drivers.filter(function (driver) {
+    for (const key in driver) {
+      if (driver[key] === value[key]){
+        return driver
+      }
+    }
+  });
+}
+
+function exactMatchToList (drivers, value) {
+  const driversMatched = exactMatch (drivers, value);
+  return driversMatched.map(function (driver) {
+    return driver.name;
+  });
+}
