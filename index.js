16e524cf9c433052b9c0ef073590ddaa467495ed
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-12-28 16:56:11 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, limit) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > limit;
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, limit) {
+  return driversWithRevenueOver(drivers, limit).map(function (driver) {
+    return driver.name;
+  });
+}
+
+function exactMatch(drivers, name) {
+  return drivers.filter(function (driver) {
+      let matches = false;
+      for (const key in name) {
+        matches = driver[key] === name[key];
+      }
+      return matches;
+    });
+}
+
+
+function exactMatchToList(drivers, name) {
+  return exactMatch(drivers, name).map(function (driver) {
+    return driver.name;
+  });
+}
