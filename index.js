2635f01b3d872ae91684582a6147979094f92d70
github username <juliannne@gmail.com>
2018-01-07 23:16:54 -0500

    Done.

--- index.js
+++ index.js
@@ -7,21 +7,19 @@ function driversWithRevenueOver(driver, revenue){
 
 function driverNamesWithRevenueOver(driver, revenue) {
   return driversWithRevenueOver(driver, revenue).map(function(person){
-    return `${person.name}`;
+    return person.name;
   });
 }
 
 function exactMatch(driver, attr) {
   return driver.filter(function(person) {
     return person[Object.keys(attr)[0]] === Object.values(attr)[0];
-  })
+  });
 }
 
 function exactMatchToList(driver, attr) {
-  return driver.filter(function(person) {
-    return person[Object.keys(attr)[0]] === Object.values(attr)[0];
-  }).
-  map(function(person) {
-    return person.name;
-  })
+  return exactMatch(driver, attr)
+    .map(function(person) {
+      return person.name;
+    });
 }
