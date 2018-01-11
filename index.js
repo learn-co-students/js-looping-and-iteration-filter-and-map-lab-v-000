b006b59bba799974b44566e14e1c28353dae6d14
dbfarms <matt.hunger@gmail.com>
2018-01-11 20:07:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue){
+    return drivers.filter(function (driver) { return driver.revenue > revenue });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+    const successfulDrivers = drivers.filter(function (driver) {return driver.revenue > revenue });
+    return successfulDrivers.map(driver => driver.name)
+}
+
+function exactMatch(drivers, matcher){
+    return drivers.filter(function (driver) { 
+        let matches = false;
+        
+        for (const key in matcher) {
+            matches = driver[key] === matcher[key];
+        }
+        return matches
+    });
+}
+
+function exactMatchToList(drivers, matcher){
+    const driverMatch = drivers.filter(function (driver) {
+        let matches = false;
+        
+        for (const key in matcher) {
+            matches = driver[key] === matcher[key];
+        }
+        return matches 
+    })
+    
+    return driverMatch.map(driver => driver.name) 
+}
