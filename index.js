07a7f837d482a730e88b6e007be21ed3bf428d59
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-20 23:21:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 // Code your solution here:
-function driversWithRevenueOver(driver,revenue){
-  return newDriver= driver.filter(function(person){return person.revenue > revenue})
+function driversWithRevenueOver(driver,amount){
+  return newDriver= driver.filter(function(person){return person.revenue > amount})
 }
-function driverNamesWithRevenueOver(driver,revenue){ //returns an array of the drivers who match it
-   names=driver.filter(function(person){return person.revenue > revenue})
+function driverNamesWithRevenueOver(driver,amount){ //returns an array of the drivers who match it
+   names=driver.filter(function(person){return person.revenue > amount})
    return names.map(function(person){return person.name})
 }
-or
-// function driverNamesWithRevenueOver(driver,revenue){
-//   return driversWithRevenueOrder(driver,revenue).map(function(person){return person[name]})
+
+// function exactMatch(driver,object){ //retuned 2 matches
+//   return driver.filter(function(person){
+//     if (person.name == object.name || person.reveue== object.revenue){
+//       return person
+//     }
+//   })
 // }
-function exactMatch(driver,object){ //retuned 2 matches
-  return driver.filter(function(person){
-    if (driver.name == object.name || driver.reveue== object.revenue){
-      return driver
-    }
-  })
-}
-or
+
 function exactMatch(driver,object){ //returned no matched
-  return driver.filter(function(person){return driver.name== object || driver.revenue == object})
+  return driver.filter(function(person){return person.name== object.name || person.revenue == object.revenue})
+}
+function exactMatchToList(driver,object){
+  filteredDriver= driver.filter(function(person){return person.name== object.name || person.revenue == object.revenue})
+  return filteredDriver.map(function(person){return person.name})
 }
