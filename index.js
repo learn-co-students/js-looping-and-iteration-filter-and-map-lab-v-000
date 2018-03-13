b5af887bf74e2a8de22f7a765114e7e701372c36
github username <github email address>
2018-03-13 12:34:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution here:
+// const drivers = [{ name: 'Sally',   revenue: 400 },
+//     { name: 'Annette', revenue: 200 },
+//     { name: 'Jim',     revenue: 150 }]
+
+function driversWithRevenueOver(list, revenue) {
+  const newList = []
+
+  list.map(function (driver) {
+    if(driver.revenue > revenue){
+      newList.push(driver);
+    }
+  });
+
+  return newList;
+}
+
+function driverNamesWithRevenueOver(list, revenue) {
+  const newList = []
+
+  list.map(function (driver) {
+    if(driver.revenue > revenue){
+      newList.push(driver.name);
+    }
+  });
+
+  return newList;
+}
+
+function exactMatch(list, obj) {
+  const newList = []
+
+  list.map(function (driver) {
+    if (driver.name === obj.name || driver.revenue === obj.revenue){
+      newList.push(driver);
+    }
+  })
+  return newList;
+}
+
+function exactMatchToList(list, obj) {
+  const newList = []
+
+  exactMatch(list, obj).map(function (driver){
+    newList.push(driver.name);
+  })
+
+  return newList;
+}
