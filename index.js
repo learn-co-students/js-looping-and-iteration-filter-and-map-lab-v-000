4e527c561fbaf1a8b097f2baf6966cddd7b2e43c
independnt <gonzalezdennis6@gmail.com>
2018-04-11 13:38:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, number){
+  return drivers.filter(function(driver){return driver.revenue > number})
+}
+
+function driverNamesWithRevenueOver(drivers, number){
+  const people = drivers.filter(function(driver){
+    return driver.revenue > number;
+  })
+  return people.map(function(person) {return person.name})
+}
+
+function exactMatch(drivers, object){
+  return drivers.filter(function(driver){return driver[Object.keys(object)[0]] === Object.values(object)[0]})
+}
+function exactMatchToList(drivers, object){
+  const match = drivers.filter(function(driver){return driver[Object.keys(object)[0]] === Object.values(object)[0]})
+  return match.map(function(driver) {return driver.name})
+}
