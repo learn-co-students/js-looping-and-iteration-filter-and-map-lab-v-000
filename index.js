2f46941c4b916ea5aa94483bb19ce7cbc07bec27
Allyson Wesman <allysonwesman@gmail.com>
2018-05-08 09:36:35 -0700

    Done.

--- index.js
+++ index.js
@@ -13,14 +13,38 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 }
 
 function exactMatch(drivers, criteria) {
-  return drivers.filter( function (driver) {
-    return driver[Object.keys(criteria)[0]] === Object.values(criteria)[0];
+
+  return drivers.filter( function(driver) {
+    let matches = [];
+
+    for (const key in criteria) {
+      if (driver[key] === criteria[key]) {
+        matches.push(true)
+      }
+      else {
+        matches.push(false) // attributes don't match
+      }
+    }
+
+    return !matches.includes(false); // only true if all criteria match
   });
 }
 
 function exactMatchToList(drivers, criteria) {
-  return drivers.filter( function (driver) {
-    return driver[Object.keys(criteria)[0]] === Object.values(criteria)[0];
+
+  return drivers.filter( function(driver) {
+    let matches = [];
+
+    for (const key in criteria) {
+      if (driver[key] === criteria[key]) {
+        matches.push(true)
+      }
+      else {
+        matches.push(false) // attributes don't match
+      }
+    }
+
+    return !matches.includes(false); // only true if all criteria match
   }).map (function (driver) {
     return driver.name
   });
