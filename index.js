dbb53ad43b0bfbb0b27d5c0ef06a108e20050ff1
nmlstyl <joey.michalina.mariano@gmail.com>
2018-06-05 16:49:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here:
+function driversWithRevenueOver(drivers, amount){
+  return drivers.filter(
+    function (entry){
+      if (entry[`revenue`] > amount){
+        return entry;
+      }
+    }
+  );
+}
+
+function driverNamesWithRevenueOver(drivers, amount){
+  let driversWhoQaulify = drivers.filter(
+    function (entry){
+      if (entry[`revenue`] > amount){
+        return entry;
+      }
+    }
+  );
+  return driversWhoQaulify.map(
+    function (entry){
+      return entry[`name`];
+    }
+  );
+}
+
+function exactMatch(drivers, object){
+    return drivers.filter(
+      function (entry){
+        if (object.name === entry.name || object.revenue === entry.revenue){
+          return entry;
+        }
+      }
+    );
+}
+
+function exactMatchToList(drivers, object){
+  let driversArray = exactMatch(drivers, object);
+  return driversArray.map(
+    function (entry){
+      return entry.name;
+    }
+  );
+}
