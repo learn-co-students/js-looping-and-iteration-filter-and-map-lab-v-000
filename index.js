c8e4b57d043cac116ab041fc115dc43955a32b12
Mel Rudick <melissarudick@MacBook-Air.local>
2018-06-07 18:05:10 -0500

    one more test to pass

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here:
+function driversWithRevenueOver(array, string) {
+  const bigEarners = array.filter(function (driver) {
+    if (driver.revenue > string) {
+    return driver
+  };
+});
+return bigEarners;
+}
+
+function driverNamesWithRevenueOver(array, string) {
+  const bigEarners = [];
+  array.filter(function (driver) {
+    if (driver.revenue > string) {
+      bigEarners.push(driver["name"]);
+    }
+  });
+  return bigEarners;
+}
+
+function exactMatch(drivers, object) {
+  const key = Object.keys(object);
+  const equalMatch = drivers.filter(function (driver) {
+    if (driver[key[0]] === object[key[0]]) {
+      return driver;
+    }
+  });
+  return equalMatch;
+}
+
+function exactMatchToList(drivers, name) {
+  debugger;
+}
