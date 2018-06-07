185eef7edd3b49ea4a82153f7860aceca5e37765
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-06-06 20:56:21 -0400

    ATP

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, targetRevenue){
+    return drivers.filter(function(el) { return el.revenue >= targetRevenue;});
+}
+
+function driverNamesWithRevenueOver(drivers, targetRevenue){
+    return driversWithRevenueOver(drivers, targetRevenue).map(function (el) {return el.name;})
+}
+
+function exactMatch(drivers, testObject){
+    return drivers.filter(function(driver) {
+        let isExactMatch = true;
+        for (key in testObject){
+            if (driver[key] !== testObject[key]){
+                isExactMatch = false;
+                break;
+            }
+        }
+        return isExactMatch;
+    });
+}
+
+function exactMatchToList(drivers, testObject){
+    return exactMatch(drivers, testObject).map(function(driver) {return driver.name;})
+}
