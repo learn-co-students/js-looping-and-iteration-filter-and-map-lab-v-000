b0cb729d87e589d6824b78012323611124ec5599
ArielFeingold <feingold.ariel@gmail.com>
2018-06-29 13:35:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-function driversWithRevenueOver(list, revenue){
-  return list.filter( function (driver) {
+
+
+
+
+function driversWithRevenueOver(drivers, revenue){
+  return drivers.filter(function(driver){
     return driver.revenue > revenue
-  });
-};
-
-function driverNamesWithRevenueOver(list, revenue){
-  return driversWithRevenueOver(list, revenue).map( function (driver) {
-    return driver.name;
-  });
-};
-
-function exactMatch (drivers, matcher) {
-  return drivers.filter(function (driver) {
-    let matches = false;
-
-    for (const key in matcher) {
-      matches = driver[key] === matcher[key];
-    }
-
-    return matches;
-  });
+  })
 }
 
-function exactMatchToList(drivers, matcher){
-  return exactMatch(drivers, matcher).map( function (driver) {
-    return driver.name;
+function driverNamesWithRevenueOver(drivers, revenue){
+  return driversWithRevenueOver(drivers, revenue).map(function(driver){
+    return driver.name
+  });
+};
+
+function exactMatch(drivers, attribute) {
+  const key = Object.keys(attribute)[0]
+  const value = Object.values(attribute)[0]
+    return drivers.filter(function(driver){
+      return driver[key] === value
+  })
+}
+
+function exactMatchToList(drivers, attribute) {
+  arr = []
+  return exactMatch(drivers, attribute).map(function(driver){
+    return driver.name
   });
 };
