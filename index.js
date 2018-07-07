820a7ee7ef9216a572a8509890fc33609123995f
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-07-07 17:20:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue) {
+  const matches = drivers.filter(x => x.revenue > revenue)
+  return matches
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  const matches = drivers.filter(x => x.revenue > revenue)
+  return matches.map(x => x.name)
+}
+
+function exactMatch(drivers, kv) {
+ return drivers.filter(x => x[Object.keys(kv)[0]] === Object.values(kv)[0] )
+}
+
+function exactMatchToList(drivers, kv) {
+  const matches = drivers.filter(x => x[Object.keys(kv)[0]] === Object.values(kv)[0] )
+  return matches.map(x => x.name)
+}
