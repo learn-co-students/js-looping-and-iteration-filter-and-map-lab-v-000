bb28c31364d867c2f99998e6899ec2d624e1daf0
Nicholas Powers <njpowers7915@gmail.com>
2018-07-24 18:23:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(i) {
+    if (i.revenue > revenue) {
+      return i;
+    };
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(i) {
+    if (i.revenue > revenue) {
+      return i.filter(function(x) {return x.name});
+    };
+  });
+}
+
+function exactMatch() {
+
+}
+
+function exactMatchToList() {
+
+}
