a028ba38d894d6f5044d6ee3f0002e5706d0d011
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-25 14:32:25 -0700

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function driversWithRevenueOver(drivers, numString){
     return drivers.filter(x => x.revenue > parseInt(numString));
 }
+
+function driverNamesWithRevenueOver(drivers, numString){
+     return drivers.filter(x => x.revenue > parseInt(numString)).map(x => x.name);
+}
+
+function exactMatch(drivers, object){
+    return drivers.filter(x => x.name === object.name || x.revenue === object.revenue)
+}
+
+function exactMatchToList(drivers, object){ 
+    return drivers.filter(x => x.name === object.name || x.revenue === object.revenue).map(x => x.name)
+}
