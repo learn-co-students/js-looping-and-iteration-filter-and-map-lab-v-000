10ebd1b36e1f5e5dcab51486d9d01a6dc6007f08
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-25 15:45:24 -0700

    corrections made with soln incorporated

--- index.js
+++ index.js
@@ -2,15 +2,55 @@
 function driversWithRevenueOver(drivers, numString){
     return drivers.filter(x => x.revenue > parseInt(numString));
 }
-
+// Remember we can call on other functions to reduce logic. DRY!! 
 function driverNamesWithRevenueOver(drivers, numString){
-     return drivers.filter(x => x.revenue > parseInt(numString)).map(x => x.name);
+     return driversWithRevenueOver(drivers, numString).map(x => x.name);
 }
+
+// bad attempt. NOT DRY
+//function driverNamesWithRevenueOver(drivers, numString){
+//     return drivers.filter(x => x.revenue > parseInt(numString)).map(x => x.name);
+// }
+
 
 function exactMatch(drivers, object){
     return drivers.filter(x => x.name === object.name || x.revenue === object.revenue)
 }
 
 function exactMatchToList(drivers, object){ 
-    return drivers.filter(x => x.name === object.name || x.revenue === object.revenue).map(x => x.name)
+    return exactMatch(drivers, object).map(x => x.name)
 }
+
+/* ----------------------FLATIRON SOLN ----------------------------------------
+function driversWithRevenueOver (drivers, revenue) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue)
+    .map(function (driver) {
+      return driver.name;
+    });
+}
+
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+
+    return matches;
+  });
+}
+
+function exactMatchToList (drivers, matcher) {
+  return exactMatch(drivers, matcher)
+    .map(function (driver) {
+      return driver.name;
+    });
+}
+*/
