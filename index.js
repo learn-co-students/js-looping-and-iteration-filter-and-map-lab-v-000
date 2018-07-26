31460bc8442d505716a6dab421fdb85bb6bc2b5f
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-25 23:37:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here:
+function driversWithRevenueOver(array, num) {
+  return array.filter(hash => hash.revenue > num);
+}
+
+function driverNamesWithRevenueOver(array, num) {
+  return driversWithRevenueOver(array, num).map(hash => hash.name);
+}
+
+function exactMatch(array, condition) {
+  return array.filter(hash => hash.name === condition.name || hash.revenue === condition.revenue);
+}
+
+function exactMatchToList(array, condition) {
+  return exactMatch(array, condition).map(hash => hash.name);
+}
