6d2057a084743939473852a80e8cdefc91ff455b
Eugene <yujunsung@gmail.com>
2018-08-09 17:55:21 -0400

    added js file

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue){
+
+
+  const result = drivers.filter(driver => driver.revenue > revenue);
+  return result;
+
+}
+
+
+function driverNamesWithRevenueOver(drivers, revenue){
+
+  const result = drivers.filter(driver => driver.revenue > revenue);
+  const names = result.map(name => name.name);
+  return names;
+}
+
+
+function exactMatch(extendedDrivers, object){
+
+  const result = extendedDrivers.filter(function(driver) {
+  for (var key in driver) {
+    if (driver[key] === object[key]){
+      return true;}
+  }
+  });
+  return result;
+
+
+
+}
+
+
+
+function exactMatchToList(extendedDrivers, object){
+
+  return exactMatch(extendedDrivers, object).map(function(driver){
+
+    return driver.name;
+
+  });
+
+
+}
