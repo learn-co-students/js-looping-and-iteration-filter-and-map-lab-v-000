49b5f9af516993bfed0543f88e6702a46661d7a4
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-20 21:42:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here:
+function driversWithRevenueOver(array, revenue) {
+  return array.filter(function(driver) {
+    return driver[`revenue`] > revenue}
+  )
+}
+
+function driverNamesWithRevenueOver(array, revenue) {
+  const driverArray = driversWithRevenueOver(array, revenue);
+  const nameArray = driverArray.map(
+    function(driver) { return driver["name"] }
+  )
+  return nameArray;
+}
