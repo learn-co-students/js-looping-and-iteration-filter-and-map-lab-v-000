9966efbe407350746c3a86e1f94f9a8ce89dca0f
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-22 14:26:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here:
+function driversWithRevenueOver(driver, revenue){
+return driver.filter(function(d){
+  return d.revenue > revenue;
+})
+}
+
+function driverNamesWithRevenueOver(driver, revenue){
+  return driver.filter(function(d){
+    if(d.revenue > revenue){
+      return d.name;
+    }
+  })
+}
