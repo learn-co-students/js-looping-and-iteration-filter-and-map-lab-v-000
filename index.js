b7f403a0f78476b5c0245499c6da6e7b173827d6
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-22 16:51:24 +0000

    Done.

--- index.js
+++ index.js
@@ -6,9 +6,31 @@ return driver.filter(function(d){
 }
 
 function driverNamesWithRevenueOver(driver, revenue){
+  const driver1 = driver.filter(function(d){
+   return d.revenue > revenue;
+ })
+  return driver1.map(function(d){
+  return  d.name
+  })
+}
+
+function exactMatch(driver,object1){
   return driver.filter(function(d){
-    if(d.revenue > revenue){
-      return d.name;
+    for (const key in object1){
+  return d[key] === object1[key];
+    }
+  })
+}
+
+function exactMatchToList(driver, object1){
+const driver1= driver.filter(function(d){
+    for(const key in object1){
+    return d[key] === object1[key];
+      }
+    })
+return driver1.map(function(d){
+    for(const key in object1){
+      return d["name"];
     }
   })
 }
