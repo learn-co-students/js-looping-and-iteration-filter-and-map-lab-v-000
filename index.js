f8fb1e17b11448022d1ce80266d2153578d58f4c
Jul Jen <juliajen.prog@gmail.com>
2018-09-05 02:58:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 
- // takes an array of driver JavaScript objects as the first argument and a string called revenue as the second argument. It returns an array of driver objects that have a revenue attribute that's greater than the passed-in revenue argument
+// takes an array of driver JavaScript objects as the first argument and a string called revenue as the second argument. It returns an array of driver objects that have a revenue attribute that's greater than the passed-in revenue argument
 
  // solution #1 refactored
 function driversWithRevenueOver(array, revenue) {
@@ -13,3 +13,10 @@ function driversWithRevenueOver(array, revenue) {
 //     return driver.revenue > revenue
 //   })
 // }
+
+
+ // returns an array of strings representing the name of each driver who has a revenue greater than the specified amount. For example, driverNamesWithRevenueOver(drivers, 2000) will return ['Sammy'], as Sammy is the name of the matching driver object with a revenue greater than 2000
+
+ function driverNamesWithRevenueOver(array, revenue) {
+   return driversWithRevenueOver(array, revenue).map(driver => driver.name)
+ }
