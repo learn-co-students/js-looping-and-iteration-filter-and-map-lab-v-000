3e8930aa0474d180820f8a42324c30c39c41429a
Chris Alberti <christopherjalberti@gmail.com>
2018-09-07 17:39:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, amount) {
+  return drivers.filter(function(driver) { return driver.revenue > amount; });
+}
+
+function driverNamesWithRevenueOver(drivers, amount) {
+  return drivers.filter(function(driver) { return driver.revenue > amount; }).
+  map(function(driver) { return driver.name; });
+}
+
+function exactMatch(drivers, conditionHash) {
+  conditionKey = Object.keys(conditionHash)[0];
+  conditionValue = conditionHash[conditionKey];
+  return drivers.filter(function(driver) { return driver[conditionKey] === conditionValue; });
+}
+
+function exactMatchToList(drivers, conditionHash) {
+  conditionKey = Object.keys(conditionHash)[0];
+  conditionValue = conditionHash[conditionKey];
+  return drivers.filter(function(driver) { return driver[conditionKey] === conditionValue; }).
+  map(function(driver) { return driver.name; });
+}
