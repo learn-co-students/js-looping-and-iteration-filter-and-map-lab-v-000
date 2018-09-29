a202124c41e868ec5a8116d342eb65981ce6cf2e
hkleindl <hkleindl@gmail.com>
2018-09-29 15:45:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, num) {
+  return drivers.filter(function(driver) { return driver.revenue > num});
+}
+
+function driverNamesWithRevenueOver(drivers, num) {
+  return driversWithRevenueOver(drivers, num).map(function(driver) {return driver.name;});
+}
+
+function exactMatch(drivers, matcher) {
+  return drivers.filter(function(driver) {
+    for (const key in matcher) {
+      return driver[key] === matcher[key];
+    }
+  });
+}
+
+function exactMatchToList(drivers, matcher) {
+  return exactMatch(drivers, matcher).map(function(driver){
+    return driver.name;});
+}
