48cd64c235cb49ea3fb6fe5226cae46e103b8cf4
sgibson47 <sgibson47@gmail.com>
2018-10-04 16:38:15 -0500

    refactor driverNamesWithRevenueOver and exactMatchToList to use driversWithRevenueOver and exactMatch respectively

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ function driversWithRevenueOver(driversObjsArr, num){
 }
 
 
-//returns an array of strings representing the name of each driver who has a revenue greater than the specified amount.
-function driverNamesWithRevenueOver(driversObjsArr, num){
-  const driverObjsWithRevenueOver = driversObjsArr.filter(function (driverObj){ 
-      return driverObj.revenue > num;
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue)
+    .map(function (driver) {
+      return driver.name;
     });
-  return driverObjsWithRevenueOver.map(function(driverObj){return driverObj.name})
 }
 
 //returns an array of the driverObjs have an attribute that matches the one passed into the function via objWithOneAttr
@@ -26,10 +25,5 @@ function exactMatch(driversObjsArr, objWithOneAttr){
 }
 //returns an array of strings representing the name of each driver who matches the passed in object
 function exactMatchToList(driversObjsArr, objWithOneAttr){
-  const key = Object.keys(objWithOneAttr)[0];
-  const value = objWithOneAttr[key]
-  const driverObjsThatMatch = driversObjsArr.filter(function (driverObj){ 
-      return driverObj[key] === value;
-    });
-  return driverObjsThatMatch.map(function(driverObj){return driverObj.name})
+  return exactMatch(driversObjsArr, objWithOneAttr).map(function(driverObj){return driverObj.name})
 }
