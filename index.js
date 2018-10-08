c89a4f7c2e418fcf8cd7658e4f1ea58dab478d4c
stazman <cadistasio@gmail.com>
2018-10-08 11:24:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here:
+
+const newDrivers = [{ name: 'Sally',   revenue: 400 },
+{ name: 'Annette', revenue: 200 },
+{ name: 'Jim',     revenue: 150 },
+{ name: 'Sally',   revenue: 200 }];
+
+function driversWithRevenueOver (drivers, revenue) {
+    return drivers.filter(function (driver) {
+        // Don't forget this return
+        if (driver.revenue > revenue) {
+            return driver;
+        }  
+    });
+}
+
+function driverNamesWithRevenueOver (drivers, revenue) {
+    return driversWithRevenueOver (drivers, revenue).map(drivers, driver {
+            return driver.name;
+    });
+}
+
+
+// don't forget that map takes in the array as first parameter, unlike filter
+
