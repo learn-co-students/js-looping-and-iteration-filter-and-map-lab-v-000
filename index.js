8eeb061c87092fc0eb6a8bc8c32ffabc276205e9
Katrina Pritchard <pritckat@gmail.com>
2018-10-12 18:39:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, amount) {
+  return drivers.filter(function(driver) {
+    return driver.revenue > amount;
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, amount) {
+  newDrivers = driversWithRevenueOver(drivers, amount);
+  return newDrivers.map(function(driver) {
+    driver.name;
+  });
+}
+
+function exactMatch() {
+
+}
