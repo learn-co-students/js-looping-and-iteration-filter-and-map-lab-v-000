54c4b51d1e34abb91d00914176117501e1ef782c
Carol Greene <carolgreene3@gmail.com>
2018-10-21 22:03:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
-  return drivers.filter(driver => driver.revenue > revenue);
+  //filter drivers to find the one's w/revenue > passed in revenue
+  return drivers.filter(driver => driver.revenue > revenue)
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
-  return driversWithRevenueOver(drivers,revenue).map (driver => driver.name)
+  //return function from above, map it & return driver.name
+  return driversWithRevenueOver(drivers, revenue).map(driver => driver.name)
 }
+//or can do
+//function driverNamesWithRevenueOver(drivers, revenue) {         //this option repeats the code from 1st
+// list = drivers.filter(driver => driver.revenue > revenue)      //function. Then maps the list & returns
+ //console.log(list)                                              //the driver name
+ //return list.map(driver => driver.name)
+ //}
+
 
 function exactMatch(drivers, object) {
-  let key = Object.keys(object)[0];
-  return drivers.filter(function (driver) {return driver[key] === object[key];});
+  //Object.keys() returns an array of a given object's own properties
+  let key = Object.keys(object)[0]
+  //console.log(key)
+  //return driver objects whose key matches the object's key
+  return drivers.filter(driver => driver[key] === object[key])
+  //console.log(list)
 }
 
+
 function exactMatchToList(drivers, object) {
-  return exactMatch(drivers, object).map (driver => driver.name)
+  //return function from above, map it & return driver.name
+  return exactMatch(drivers, object).map(driver => driver.name)
 }
