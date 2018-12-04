c66876cc8e48c76b21c7d911ee6221fc48563339
jonathan <jonathanyi81@gmail.com>
2018-12-04 00:11:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here:
+/*
+map - new array with each element being the result of the callback
+filter - new array with the elements that pass the test
+*/
+function driversWithRevenueOver(arr, revenue) {
+  return arr.filter(
+    function(driver) {
+      return driver.revenue > revenue
+    }
+  )
+}
+
+function driverNamesWithRevenueOver(arr, revenue) {
+  return driversWithRevenueOver(arr, revenue).map(driver => driver.name)
+}
+
+function exactMatch(arr, obj) {
+  return arr.filter(
+    function(driver) {
+      const match = Object.keys(obj)
+      /*
+      console.log(match[0])
+      console.log(driver[match[0]])
+      */
+      return (driver[match[0]] === obj[match[0]])
+    }
+  )
+}
+
+function exactMatchToList(arr, obj) {
+  return exactMatch(arr, obj).map(driver => driver.name)
+
+}
