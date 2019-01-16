54c634b236e7d840db73d3ca05a477be426d13e5
Kayla Cross <kac288@georgetown.edu>
2019-01-16 12:59:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, money){
+
+  let newCollection = [];
+
+  for (const driver of drivers){
+    if (driver.revenue > money){
+      newCollection.push(driver);
+    }
+  }
+
+  return newCollection;
+
+}
+
+function driverNamesWithRevenueOver(drivers, money){
+
+  let newCollection = [];
+
+  for (const driver of drivers){
+    if (driver.revenue > money){
+      newCollection.push(driver.name);
+    }
+  }
+
+  return newCollection;
+}
+
+
+function exactMatch(drivers, condition){
+
+  return drivers.filter(function(driver) {
+    let matches = false;
+    for(const c in condition){
+      matches = driver[c] === condition[c];
+    }
+    return matches;
+  });
+
+}
+
+function exactMatchToList(drivers, condition){
+
+  let rtnDrivers = drivers.filter(function(driver) {
+    let matches = false;
+    for(const c in condition){
+      matches = driver[c] == condition[c];
+    }
+    return matches;
+  });
+
+  return rtnDrivers.map(d => d.name);
+
+}
