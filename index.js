9e3718ed64dfbcc4632bdd24fc1866137544a713
lauradarlak <lauradarlak@gmail.com>
2019-01-20 13:17:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 // Code your solution here:
 
-function driversWithRevenueOver(list, revenue){
-  return list.filter(function(driverRevenue) {
-    return driverRevenue.revenue > revenue;
+function driversWithRevenueOver(drivers, revenue){
+  // filter drivers with revenues greater than revenue arg
+  return drivers.filter(function(driver) {
+    return driver.revenue > revenue;
   });
-
 }
 
-function driverNamesWithRevenueOver(list, revenue) {
+function driverNamesWithRevenueOver(drivers, revenue) {
   // filter drivers with revenues greater than revenue arg
-   return list.filter(driver => {
-    return driver.revenue > revenue;
-  })
+  return driversWithRevenueOver(drivers, revenue)
   // map out those drivers' names
   .map(driver => driver.name)
-
 }
 
-function exactMatch(list, criteria){
-  return list.filter(function(obj) {
-    return Object.keys(criteria).every(function(c) {
-      return obj[c] == criteria[c];
-    });
+function exactMatch(drivers, criteria){
+  return drivers.filter(function(driver) {
+    let match = false;
+
+    for(const key in criteria) {
+      match = driver[key] === criteria[key];
+    }
+    return match;
   });
 }
 
-function exactMatchToList(list, criteria){
-  return list.filter(function(obj) {
-    return Object.keys(criteria).every(function(c) {
-      return obj[c] == criteria[c];
-    })
-  })
-  .map(obj => obj.name)
+function exactMatchToList(drivers, criteria){
+  return exactMatch(drivers, criteria)
+  .map(driver => driver.name)
 }
