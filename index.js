0598370d4f28f99416b0a7b213b73ea0461820cd
Richard-Frost <frost.rchrd@gmail.com>
2019-01-25 21:07:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution here:
+function driversWithRevenueOver(array, revenue) { 
+	return array.filter(function(driver) { 
+		return driver.revenue >= revenue 
+	});
+}
+ 
+ function driverNamesWithRevenueOver1(array, revenue) {
+
+ 	return array.map(function(driver) { 
+ 		if (driver.revenue >= revenue) { return driver.name }
+ 		});
+
+
+ 	return driverList	
+ 	}
+
+
+ 	function driverNamesWithRevenueOver(array, revenue) {
+ 		return driversWithRevenueOver(array, revenue).map (function(driver) { 
+ 				return driver.name
+ 			});
+ 		}
+
+ 	
+
+ function exactMatch (drivers, thing) { 
+ 	return drivers.filter(function(driver) { debugger; 
+ 		for (const key in driver) {  }
+            
+    });
+ }
+  
+
+
+function aexactMatch(array, thing) { 
+  return array.filter(function(driver) {  
+    
+      debugger;
+    });     
+  }
+function exactMatch (drivers, thing) {
+  return drivers.filter(function (driver) {
+   
+    for (key in thing) { return thing[key] === driver[key] }
+    	debugger;   
+  });
+}
+
+function exactMatchToList (drivers, thing) {
+  	return exactMatch(drivers, thing).map(function(driver) { return driver.name});
+   
+}
+
