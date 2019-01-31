905cfd2cc370a95c264a9a4afeb63dd468159a0d
freeWillee <will.ik.lee@gmail.com>
2019-01-31 14:49:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, amount) {
-    const filteredDrivers =  drivers.filter(function(driver) {
+   return drivers.filter(function(driver) {
         return driver.revenue > amount;
     });
-
-    return filteredDrivers;
 }
 
 function driverNamesWithRevenueOver(drivers, amount) {
-    const filteredDrivers = driversWithRevenueOver(drivers, amount);
-
-    const driverNames = filteredDrivers.map(function(driver) {
+    return driversWithRevenueOver(drivers, amount).map(function(driver) {
         return driver.name;
     })
-
-    return driverNames;
 }
 
-function exactMatch(drivers, queryObject) {
-    const matchedDriver = drivers.filter(function(driver) {
-        return driver[Object.keys(queryObject)[0]] === queryObject[Object.keys(queryObject)[0]]
+function exactMatch(drivers, matcher) {
+    // return matchedDriver = drivers.filter(function(driver) {
+    //     return driver[Object.keys(matcher)[0]] === matcher[Object.keys(matcher)[0]]
+    // });
+
+    return drivers.filter(function(driver) { 
+        let matches = false;
+
+        for (const key in matcher) {
+            matches = driver[key] === matcher[key];
+        }
+
+        return matches;
     });
-
-    return matchedDriver;
 }
 
-function exactMatchToList(drivers, queryObject) {
-    const matchedDrivers = exactMatch(drivers, queryObject)
+function exactMatchToList(drivers, matcher) {
+    // const matchedDrivers = exactMatch(drivers, matcher)
 
-    return matchedDrivers.map(function(driver) {return driver.name});
+    return exactMatch(drivers, matcher).map(function(driver) {return driver.name});
 }
