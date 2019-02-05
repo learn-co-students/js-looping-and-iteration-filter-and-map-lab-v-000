abfbfde1ad5b81cca4b34f3fe775cb8ef4120650
Taylor Williams <taylor@thewilliams.net>
2019-02-05 03:52:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue){
+  return drivers.filter(driver => driver.revenue > revenue)
+}
+function driverNamesWithRevenueOver(drivers, revenue){
+  const newDrivers = driversWithRevenueOver(drivers, revenue)
+  return newDrivers.map(driver => driver.name)
+}
+function exactMatch(drivers, matcher){
+  return drivers.filter(function (driver) {
+    let matches = false
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key]
+    }
+    return matches
+  })
+}
+function exactMatchToList(drivers, matcher){
+  const newDrivers = exactMatch(drivers, matcher)
+  return newDrivers.map(driver => driver.name)
+}
