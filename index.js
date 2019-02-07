317f4858884b75232deeb89c5d96245f87a231b6
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-07 12:55:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here:
+
+
+function driversWithRevenueOver(drivers, revenue) {
+      return drivers.filter( function(driver) {
+        return driver.revenue > revenue;
+      });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map( function(driver) {
+        return driver.name;
+  });
+}
+
+function exactMatch(drivers, match) {
+  return drivers.filter( function(driver) {
+    for (const key in match){
+      return (driver[key] === match[key])
+    }
+  });
+}
+
+function exactMatchToList(drivers, match) {
+  return exactMatch(drivers, match).map( function(driver) {
+    return driver.name;
+  });
+}
