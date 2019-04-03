0f586831825cab5c85af1fe2092cacee7cd89b2a
Josely Castro <joselycas@gmail.com>
2019-04-03 10:09:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 
 function driversWithRevenueOver(driver, revenue) {
-  driver.filter(function (name) { return name.revenue > revenue; });
-  
+  return driver.filter(function(name) { return name.revenue > revenue; });
+
 }
+
+function driverNamesWithRevenueOver(driver, revenue){
+  return driversWithRevenueOver(driver, revenue).map(function (name) { return name.name; });
+
+}
+
+function exactMatch(driver, revenue){
+  const arg_keys = Object.keys(revenue)
+  debugger
+  return driver.filter(function(name) {return name[arg_keys] === revenue[arg_keys]; });
+}
+
+
+ function exactMatchToList(driver, revenue){
+   return exactMatch(driver, revenue).map(function (name) {return name.name;});
+
+ }
