3b57e712b10eadf94cd8a8f108c93cbe6ec97dfe
Samantha brake <sammy.brake@gmail.com>
2019-05-17 22:37:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver) {
+     if (driver['revenue'] >= revenue) {
+      return driver;
+    };
+  });
+};
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  newDrivers = [];
+  drivers.filter(function(driver) {
+     if (driver['revenue'] >= revenue) {
+      newDrivers.push(driver['name']);
+    };
+  });
+  return newDrivers;
+};
