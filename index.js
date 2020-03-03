615b574d626fadd20d1efa8bdfc6e9b6e863e09f
RonSala <ron.sala@gmail.com>
2020-03-03 17:55:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(e => e.revenue > revenue);
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return drivers.filter(e => e.revenue > revenue)
+  .map(e => e.name);
+}
